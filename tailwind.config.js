9c19a0eadbb7f3963c153a7486a3a672859a6536
Kedar <kedarmakode1598@gmail.com>
2022-08-06 18:50:12 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      lg:'1440px'
+    },
+    extend: {
+      colors:{
+        primary:'#5955b3'
+      }
+    },
+  },
+  plugins: [],
+}
